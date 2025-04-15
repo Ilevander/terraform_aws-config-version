3a522a4e2005670c61b9cf92e90bd4aa98f5952e
dell <dell@DESKTOP-4P8U8AR>
2025-04-15 05:10:39 +0100

    Docs , Comments

--- main.tf
+++ main.tf
@@ -1,4 +1,5 @@
-
+//Specifying the required Terraform version (~> 1.7). Additionally, defining the required providers and their versions. 
+//In this case, aws provider (~> 5.0) and the random provider (~> 3.0).
 terraform {
   required_version = "~> 1.7"
   required_providers {
@@ -13,21 +14,22 @@ terraform {
   }
 }
 
-
+//provider block to configure the AWS region to "eu-west-1".
 provider "aws" {
   region = "eu-west-1"
 }
 
-
+//random_id resource named "bucket_suffix" with a byte_length of 6. This ID will be used to create a unique name for the S3 bucket.
 resource "random_id" "bucket_suffix" {
   byte_length = 6
 }
 
-
+//aws_s3_bucket resource named "example_bucket". Using interpolation to create a unique bucket name by appending the random ID to "example-bucket-".
 resource "aws_s3_bucket" "example_bucket" {
   bucket = "example-bucket-${random_id.bucket_suffix.hex}"
 }
 
+//output block to output the name of the created bucket.
 output "bucket_name" {
   value = aws_s3_bucket.example_bucket.bucket
 }
